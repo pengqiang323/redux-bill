0db59a7f6a61f4a1dd22e22b31bcbd631806ac9b
pengqiang <pengqiang@tzcpa.com>
2024-12-30 17:08:34 +0800

    完成主题色的配置

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,9 +1,15 @@
+import { Button } from 'antd-mobile'
 import {Outlet} from 'react-router-dom'
 const Layout = ()=>{
     return (
         <div>
             <Outlet/>
             我是Layout
+
+            <Button color='primary'>测试全局</Button>
+            <div className='puple'>
+                <Button color='primary'>测试全局</Button>
+            </div>
         </div>
 
     )
